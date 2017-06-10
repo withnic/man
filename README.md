e40af531e41bf0d064c86d8e9daa646ce0295569
Kawamoto norifumi <mybest.is.next.one@gmail.com>
2017-06-10 17:31:28 +0900

    get meta info : title and description

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # maw
-enhancement man
+enhancement man command
 
-It 's man command added whois.
+It 's man command added web info.
+
+# usage
+
+```
+# default man command
+$./maw less 
+
+# title and Description
+$./maw https://stackoverflow.com                                                                                 
+Page Infomation: [ページメタ情報]
+Title :				Stack Overflow
+Description :			Stack Overflow is the largest online community for programmers to learn, share their knowledge, and advance their careers
+```
--- main.go
+++ main.go
@@ -1,43 +1,72 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"net/url"
 	"os"
 	"os/exec"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
 	os.Exit(run(os.Args))
 }
 
+// webinfo prints page's title and description
+func webinfo(url string) int {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		fmt.Printf("goquery Error:%s\n", err.Error())
+		return 1
+	}
+	fmt.Fprint(os.Stdout, "Page Infomation: [ページメタ情報]\n")
+	doc.Find("head").Each(func(i int, s *goquery.Selection) {
+		fmt.Fprint(os.Stdout, fmt.Sprintf("Title :\t\t\t\t%s\n", s.Find("title").Text()))
+		s.Find("meta").Each(func(i int, s *goquery.Selection) {
+			if name, _ := s.Attr("name"); name == "description" {
+				description, _ := s.Attr("content")
+				fmt.Fprint(os.Stdout, fmt.Sprintf("Description :\t\t\t%s\n", description))
+			}
+		})
+	})
+	return 0
+}
+
+// man runs man command.
+func man(options []string) int {
+	cmd := exec.Command("man", options...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("man Error:%s\n", err.Error())
+		return 1
+	}
+	return 0
+}
+
+// transeformURL returns url string
+func transeformURL(str string) (string, error) {
+	u, err := url.Parse(str)
+	if u.Host != "" {
+		return str, err
+	}
+	return "url parse error", errors.New("str is not url format")
+}
+
 func run(args []string) int {
-	var out []byte
-	var err error
 	if len(args) < 2 {
 		fmt.Printf("What manual page do you want?")
 		return 1
 	}
 
-	u, err := url.Parse(args[1])
+	url, err := transeformURL(args[1])
 
-	if u.Host != "" {
-		out, err = exec.Command("whois", u.Host).Output()
-		if err != nil {
-			log.Fatal(err)
-			return 1
-		}
-		fmt.Fprint(os.Stdout, string(out))
-		return 0
-	}
-
-	out, err = exec.Command("man", args[1:]...).Output()
 	if err != nil {
-		log.Fatal(err)
-		return 1
+		return man(args[1:])
 	}
-
-	fmt.Fprint(os.Stdout, string(out))
-	return 0
+	return webinfo(url)
 }
