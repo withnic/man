141a4a24a9a7eeefd91f4b074201cce6985f6c94
Kawamoto norifumi <mybest.is.next.one@gmail.com>
2017-06-16 20:14:33 +0900

    trans url test

--- main_test.go
+++ main_test.go
@@ -1,1 +1,15 @@
 package main
+
+import "testing"
+
+func TestTranseformURL(t *testing.T) {
+	url, err := transeformURL("http://localhost.com")
+	if err != nil {
+		t.Errorf("has error :%s", err.Error())
+	}
+
+	expected := "http://localhost.com"
+	if url != expected {
+		t.Errorf("got %s expected %s", url, expected)
+	}
+}
